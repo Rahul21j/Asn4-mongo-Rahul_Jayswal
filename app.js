728f6a764226e2b35922d43777b504fa87accc26
Rahul <rahul1353rkj@gmail.com>
2024-04-03 11:58:36 -0400

    Changed port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var database = require('./config/database');
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
 const exphbs = require("express-handlebars");
  
-var port     = process.env.PORT || 3000;
+var port     = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
